766cfa8c69b5a00ace42c492e02bc8efb25bb4f7
Duy Le <dtl11@sfu.ca>
2020-11-15 15:02:38 -0800

    config for user pool

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_oeEbWt3dT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '25oso11um4o4d7c4pk4r1odpn7g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1 ' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
