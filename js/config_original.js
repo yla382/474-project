9c81a95c40e5d09d30f94ff08b63abba2576d0eb
Duy Le <dtl11@sfu.ca>
2020-11-22 16:05:02 -0800

    changed api gateway

--- js/config_original.js
+++ js/config_original.js
@@ -1,3 +1,4 @@
+/*
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_qCtBceIuU',
@@ -6,5 +7,16 @@ window._config = {
     },
     api: {
         invokeUrl: 'https://dvjfmnucag.execute-api.us-east-1.amazonaws.com/prod'
+    }
+};*/
+
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_y59AZLfLV',
+        userPoolClientId: '3inqtah42lk35b174sjjntmauc',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://gvqkruled4.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
